d91f6af0eed26fa6b8118b2c0de70af421c057ad
Muhammad Hassan <hassandarwaish0@gmail.com>
2025-01-21 19:23:43 +0500

    Incompleted

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:ecommerceapp/app/app.locator.dart';
 import 'package:ecommerceapp/app/app.router.dart';
+import 'package:ecommerceapp/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await setupLocator();
   runApp(const MyApp());
 }
